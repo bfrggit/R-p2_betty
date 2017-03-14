d56e4ee311519525e8ccbaaea6f4798d94e696a0
Charles Zhu <zhuqiuxiuci@gmail.com>
2017-03-13 17:26:37 -0700

    New definitions of methods in SquareCellGrid

--- square_cell_grid.R
+++ square_cell_grid.R
@@ -1,72 +1,168 @@
+# CLASS DEFINITION OF SquareCellGrid
 setClass(
     "SquareCellGrid",
     
-    slots = c(
-        cell_len_x = "numeric",
-        cell_len_y = "numeric",
-        numbering = "matrix",
-        num_offset_x = "integer",
-        num_offset_y = "integer"
+    representation(
+        num_cells_1 = "integer",
+        num_cells_2 = "integer",
+        num_offset_1 = "integer",
+        num_offset_2 = "integer",
+        cell_len_x = "numeric",         # WE direction length
+        cell_len_y = "numeric",         # NS direction length
+        numbering = "matrix"
     ),
     
     prototype = list(
         numbering = matrix(NA, 0, 0)
-    )
+    ),
+    
+    validity = function(object) {
+        if(object@num_cells_1 <= 0)     return(FALSE)
+        if(object@num_cells_2 <= 0)     return(FALSE)
+        if(object@cell_len_x <= 0)      return(FALSE)
+        if(object@cell_len_y <= 0)      return(FALSE)
+        if(nrow(object@numbering) != object@num_cells_1)    return(FALSE)
+        if(ncol(object@numbering) != object@num_cells_2)    return(FALSE)
+        TRUE
+    }
 )
 
-# constructor
-SquareCellGrid = function(
-    num_cells_x, num_cells_y,
-    cell_len_x, cell_len_y,
-    num_offset_x = 0, num_offset_y = 0
-) {
-    stopifnot(num_cells_x == as.integer(num_cells_x))
-    stopifnot(num_cells_y == as.integer(num_cells_y))
-    stopifnot(num_offset_x == as.integer(num_offset_x))
-    stopifnot(num_offset_y == as.integer(num_offset_y))
-    stopifnot(num_cells_x > 0)
-    stopifnot(num_cells_y > 0)
-    # stopifnot(num_offset_x > 0)
-    # stopifnot(num_offset_y > 0)
-    
-    mat_numbering = matrix(
-        1:(num_cells_x * num_cells_y),
-        nrow = num_cells_y,
-        ncol = num_cells_x,
-        byrow = TRUE
-    )
-    new("SquareCellGrid",
-        cell_len_x = cell_len_x,
-        cell_len_y = cell_len_y,
-        num_offset_x = as.integer(num_offset_x),
-        num_offset_y = as.integer(num_offset_y),
-        numbering = mat_numbering
-    )
-}
-
+# CONSTRUCTOR OF SquareCellGrid
 setGeneric(
-    name = "cord_to_num",
-    def = function(object, cord_x, cord_y) {
-        standardGeneric("cord_to_num")
+    "SquareCellGrid",
+    
+    valueClass = "SquareCellGrid",
+    
+    function(
+        num_cells_1,
+        num_cells_2,
+        cell_len_x,
+        cell_len_y,
+        num_offset_1,
+        num_offset_2
+    ) {
+        standardGeneric("SquareCellGrid")
     }
 )
 
 setMethod(
-    f = "cord_to_num",
-    signature = "SquareCellGrid",
-    definition = function(object, cord_x, cord_y) {
-        # object
-        # cord_x    numeric
-        # cord_y    numeric
-        bnd_lt = -object@num_offset_x * object@cell_len_x
-        bnd_tp = -object@num_offset_y * object@cell_len_y
-        stopifnot(cord_x > bnd_lt)
-        stopifnot(cord_y > bnd_tp)
-        stopifnot(cord_x < bnd_lt + ncol(object@numbering) * object@cell_len_x)
-        stopifnot(cord_y < bnd_tp + nrow(object@numbering) * object@cell_len_y)
+    "SquareCellGrid",
+    
+    signature(
+        num_cells_1 = "integer",
+        num_cells_2 = "integer",
+        cell_len_x = "numeric",
+        cell_len_y = "numeric",
+        num_offset_1 = "integer",
+        num_offset_2 = "integer"
+    ),
+
+    function(
+        num_cells_1,
+        num_cells_2,
+        cell_len_x,
+        cell_len_y,
+        num_offset_1,
+        num_offset_2
+    ) {
+        stopifnot(num_cells_1 > 0)
+        stopifnot(num_cells_2 > 0)
         
-        c_x = (cord_x - bnd_lt) / object@cell_len_x + 1
-        c_y = (cord_y - bnd_tp) / object@cell_len_y + 1
-        return(object@numbering[c_y, c_x])
+        mat_numbering = matrix(
+            1:(num_cells_1 * num_cells_2),
+            nrow = num_cells_1,
+            ncol = num_cells_2,
+            byrow = TRUE
+        )
+        new("SquareCellGrid",
+            num_cells_1 = num_cells_1,
+            num_cells_2 = num_cells_2,
+            num_offset_1 = num_offset_1,
+            num_offset_2 = num_offset_2,
+            cell_len_x = cell_len_x,
+            cell_len_y = cell_len_y,
+            numbering = mat_numbering
+        ) # RETURN
+    }
+)
+
+# METHODS OF SquareCellGrid
+setGeneric(
+    "x_y_to_cell_1_2",
+    
+    valueClass = "integer",
+    
+    function(object, x, y) {
+        standardGeneric("x_y_to_cell_1_2")
+    }
+)
+
+setMethod(
+    "x_y_to_cell_1_2",
+    
+    signature(
+        object = "SquareCellGrid",
+        x = "numeric",
+        y = "numeric"
+    ),
+    
+    function(object, x, y) {
+        bnd_lt = -object@num_offset_1 * object@cell_len_x
+        bnd_tp = -object@num_offset_2 * object@cell_len_y
+        stopifnot(x > bnd_lt)
+        stopifnot(y > bnd_tp)
+        stopifnot(x < bnd_lt + ncol(object@numbering) * object@cell_len_x)
+        stopifnot(y < bnd_tp + nrow(object@numbering) * object@cell_len_y)
+        c_2 = as.integer((x - bnd_lt) / object@cell_len_x) + 1L
+        c_1 = as.integer((y - bnd_tp) / object@cell_len_y) + 1L
+        c(c_1, c_2) # RETURN
+    }
+)
+
+setGeneric(
+    "cell_1_2_to_num",
+    
+    valueClass = "integer",
+    
+    function(object, c_1, c_2) {
+        standardGeneric("cell_1_2_to_num")
+    }
+)
+
+setMethod(
+    "cell_1_2_to_num",
+    
+    signature(
+        c_1 = "integer",
+        c_2 = "integer"
+    ),
+    
+    function(object, c_1, c_2) {
+        object@numbering[c_1, c_2] # RETURN
+    }
+)
+
+setGeneric(
+    "x_y_to_cell_num",
+    
+    valueClass = "integer",
+    
+    function(object, x, y) {
+        standardGeneric("x_y_to_cell_num")
+    }
+)
+
+setMethod(
+    "x_y_to_cell_num",
+    
+    signature(
+        object = "SquareCellGrid",
+        x = "numeric",
+        y = "numeric"
+    ),
+    
+    function(object, x, y) {
+        c = x_y_to_cell_1_2(object, x, y)
+        cell_1_2_to_num(object, c[1], c[2])
     }
 )
