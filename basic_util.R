0c35e3f794b0b798fda6a64e4537eb4c4882ca8d
Qiuxi Zhu <zhuqiuxiuci@gmail.com>
2017-03-28 19:24:28 -0700

    Basic utilities

---
+++ basic_util.R
@@ -0,0 +1,82 @@
+# RETURN z_ notation strings
+z_nd_str = function(str_z, val_n) {
+    stopifnot(is.character(str_z))
+    stopifnot(is.integer(val_n))
+    stopifnot(val_n > 0)
+    stopifnot(length(str_z) == 1)
+    
+    paste(str_z, as.character(1:val_n), sep = "_") # RETURN
+}
+
+# GENERATE RANDOM sensing capacity matrix for all nodes
+get_capacity_mat_rand = function(val_n, val_k, p) {
+    stopifnot(is.integer(val_n))
+    stopifnot(is.integer(val_k))
+    stopifnot(val_n > 0)
+    stopifnot(val_k > 0)
+    stopifnot(is.numeric(p))    # p is the probablity of ONE assignments
+    stopifnot(p > 0 && p <= 1)
+    
+    mat_c = matrix(
+        0,
+        nrow = val_n,
+        ncol = val_k,
+        byrow = TRUE
+    )
+    rownames(mat_c) = z_nd_str("n", val_n)
+    colnames(mat_c) = z_nd_str("d", val_k)
+    c_t = runif(val_n * val_k, min = 0, max = 1)
+    c_p = ifelse(c_t > p, 0, 1)
+    mat_c[] = c_p
+    mat_c # RETURN
+}
+
+# GENERATE RANDOM data type specs
+get_data_type_spec_df_rand = function(val_k, r_max) {
+    stopifnot(is.integer(val_k))
+    stopifnot(val_k > 0)
+    stopifnot(is.numeric(r_max))    # r_max is the maximum data rate
+    stopifnot(r_max > 0)
+
+    df_d = data.frame(
+        rep(0, val_k),
+        z_nd_str("s_impact", val_k),
+        z_nd_str("t_impact", val_k),
+        row.names = z_nd_str("d", val_k),
+        check.names = TRUE,
+        fix.empty.names = TRUE
+    )
+    colnames(df_d) = c("rate", "s_impact_f", "t_impact_f")
+    df_d[,"rate"] = runif(val_k, min = 0, max = r_max)
+    df_d
+}
+
+# GENERATE LOCAL ONLY impact functions
+make_impact_f_local_only = function(val_k) {
+    for(knd in 1:val_k) {
+        do.call(
+            "<<-",
+            list(
+                paste("s_impact_f", knd, sep = "_"),
+                function(x) {
+                    stopifnot(is.numeric(x))
+                    stopifnot(x >= 0)
+                    
+                    ifelse(x > 0, 0, 1)
+                }
+            )
+        )
+        do.call(
+            "<<-",
+            list(
+                paste("t_impact_f", knd, sep = "_"),
+                function(x) {
+                    stopifnot(is.numeric(x))
+                    stopifnot(x >= 0)
+                    
+                    ifelse(x > 0, 0, 1)
+                }
+            )
+        )
+    }
+}
--- dummy_util.R
+++ dummy_util.R
@@ -1,17 +1,20 @@
 get_placement_mat_rand = function(val_n, val_m) {
-    stopifnot(val_n == as.integer(val_n))
-    stopifnot(val_m == as.integer(val_m))
+    stopifnot(is.integer(val_n))
+    stopifnot(is.integer(val_m))
     stopifnot(val_n > 0)
     stopifnot(val_m > 0)
+    
     mat_g = matrix(
         0,
-        nrow = val_n, ncol = val_m, byrow = TRUE
+        nrow = val_n,
+        ncol = val_m,
+        byrow = TRUE
     )
+    rownames(mat_g) = paste("n", as.character(1:val_n), sep = "_")
+    colnames(mat_g) = paste("c", as.character(1:val_m), sep = "_")
     placement_per_node = as.integer(runif(val_n, min = 1, max = val_m + 1))
     for(jnd in 1:val_n) {
         mat_g[jnd, placement_per_node[jnd]] = 1
     }
-    rownames(mat_g) = paste("n", as.character(1:val_n), sep = "_")
-    colnames(mat_g) = paste("c", as.character(1:val_m), sep = "_")
-    mat_g
+    mat_g # RETURN
 }
