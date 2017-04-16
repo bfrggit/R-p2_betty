4270aff63b6ae004074a095e99af86ab10d210a0
Qiuxi Zhu <zhuqiuxiuci@gmail.com>
2017-04-16 00:47:49 -0700

    Updated roma/taxi script

--- misc/txt_parse_roma_taxi.rb
+++ misc/txt_parse_roma_taxi.rb
@@ -5,7 +5,7 @@ while str = gets
 	id = ln[0].to_i
 	t = DateTime.parse(ln[1]).to_time.to_f
 	c_match = ln[2].match /^POINT\(([\d\.\+\-]+)\s([\d\.\+\-]+)\)$/
-	lat = c_match[1].to_f
-	lon = c_match[2].to_f
+	lat = c_match[1]
+	lon = c_match[2]
 	puts "#{id} #{t} #{lat} #{lon}"
 end
