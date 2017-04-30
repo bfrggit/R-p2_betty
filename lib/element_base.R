2d1193095d2b72ce94776699ebde230890231203
Qiuxi Zhu <zhuqiuxiuci@gmail.com>
2017-04-29 19:41:31 -0700

    Improved make_impact func to support vector of knd

--- lib/element_base.R
+++ lib/element_base.R
@@ -32,15 +32,15 @@ if(!exists("EX_ELEMENT_BASE_R")) {
 #     NA # NO RETURN
 # }
 
-# GENERATE impact functions using general getters
-make_s_impact_f_type <<- function(type, val_k, ...) {
+# GENERATE impact functions using general getters for selected data types
+make_s_impact_f_type_vec <<- function(type, kcl, ...) {
     stopifnot(is.character(type))
     stopifnot(length(type) == 1)
-    stopifnot(is.integer(val_k))
-    stopifnot(length(val_k) == 1L)
-    stopifnot(val_k > 0L)
+    stopifnot(is.integer(kcl))
+    stopifnot(length(kcl) > 0L)
+    stopifnot(kcl > 0L)
 
-    for(knd in 1L:val_k) {
+    for(knd in kcl) {
         do.call(
             "<<-", list(
                 paste("s_impact_f", knd, sep = "_"),
@@ -52,14 +52,14 @@ make_s_impact_f_type <<- function(type, val_k, ...) {
     NA # NO RETURN
 }
 
-make_t_impact_f_type <<- function(type, val_k, ...) {
+make_t_impact_f_type_vec <<- function(type, kcl, ...) {
     stopifnot(is.character(type))
     stopifnot(length(type) == 1)
-    stopifnot(is.integer(val_k))
-    stopifnot(length(val_k) == 1L)
-    stopifnot(val_k > 0L)
+    stopifnot(is.integer(kcl))
+    stopifnot(length(kcl) > 0L)
+    stopifnot(kcl > 0L)
 
-    for(knd in 1L:val_k) {
+    for(knd in kcl) {
         do.call(
             "<<-", list(
                 paste("t_impact_f", knd, sep = "_"),
@@ -69,6 +69,61 @@ make_t_impact_f_type <<- function(type, val_k, ...) {
     }
 
     NA # NO RETURN
+}
+
+# GENERATE impact functions using general getters
+# make_s_impact_f_type <<- function(type, val_k, ...) {
+#     stopifnot(is.character(type))
+#     stopifnot(length(type) == 1)
+#     stopifnot(is.integer(val_k))
+#     stopifnot(length(val_k) == 1L)
+#     stopifnot(val_k > 0L)
+#
+#     for(knd in 1L:val_k) {
+#         do.call(
+#             "<<-", list(
+#                 paste("s_impact_f", knd, sep = "_"),
+#                 get_impact_f_type(type, ...)
+#             )
+#         )
+#     }
+#
+#     NA # NO RETURN
+# }
+#
+# make_t_impact_f_type <<- function(type, val_k, ...) {
+#     stopifnot(is.character(type))
+#     stopifnot(length(type) == 1)
+#     stopifnot(is.integer(val_k))
+#     stopifnot(length(val_k) == 1L)
+#     stopifnot(val_k > 0L)
+#
+#     for(knd in 1L:val_k) {
+#         do.call(
+#             "<<-", list(
+#                 paste("t_impact_f", knd, sep = "_"),
+#                 get_impact_f_type(type, ...)
+#             )
+#         )
+#     }
+#
+#     NA # NO RETURN
+# }
+
+make_s_impact_f_type <<- function(type, val_k, ...) {
+    stopifnot(is.integer(val_k))
+    stopifnot(length(val_k) == 1L)
+    stopifnot(val_k > 0L)
+
+    make_s_impact_f_type_vec(type, 1L:val_k, ...)
+}
+
+make_t_impact_f_type <<- function(type, val_k, ...) {
+    stopifnot(is.integer(val_k))
+    stopifnot(length(val_k) == 1L)
+    stopifnot(val_k > 0L)
+
+    make_t_impact_f_type_vec(type, 1L:val_k, ...)
 }
 
 # GENERATE LOCAL ONLY impact functions
