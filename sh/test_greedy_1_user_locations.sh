002cd1e218cc5296e6e2b53a9fbcdc93a5445adb
Qiuxi Zhu <qiuxiz@ics.uci.edu>
2017-05-08 13:27:35 -0700

    Minor fix in sh script

--- sh/test_greedy_1_user_locations.sh
+++ sh/test_greedy_1_user_locations.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-di=`pwd`
-ts=`date +%s`
 cd ~/proj/R-p2/
-Rscript \
-	-o "${di}/test_greedy_1_user_locations_${ts}_out.log" \
-	-e "${di}/test_greedy_1_user_locations_${ts}_err.log" \
-	test_greedy_1_user_locations.R
+Rscript test_greedy_1_user_locations.R
