31babeed3b95b6146f4ced0c00499d1aac06442d
Qiuxi Zhu <zhuqiuxiuci@gmail.com>
2017-05-08 12:41:24 -0700

    Using manually set output files

--- sh/test_greedy_1_user_locations.sh
+++ sh/test_greedy_1_user_locations.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+di=`pwd`
+ts=`date +%s`
 cd ~/proj/R-p2/
-Rscript test_greedy_1_user_locations.R
+Rscript \
+	-o "${di}/test_greedy_1_user_locations_${ts}_out.log" \
+	-e "${di}/test_greedy_1_user_locations_${ts}_err.log" \
+	test_greedy_1_user_locations.R
