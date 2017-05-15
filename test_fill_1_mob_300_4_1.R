3dd6794596bd47ea24d5c7f69e7c7d15271ea45d
Qiuxi Zhu <zhuqiuxiuci@gmail.com>
2017-05-15 01:12:07 -0700

    Using prepared RData files for test_mob

--- test_fill_1_mob_300_4_1.R
+++ test_fill_1_mob_300_4_1.R
@@ -4,33 +4,25 @@ cat("\n")
 
 require(methods)
 
+source("lib/square_cell_grid.R")
+source("lib/element_multi.R")
+source("lib/placement_user_locations.R")
+
 # simulation CONSTANTS
-num_cells_1 = 7L
-num_cells_2 = 9L
-cell_len_x = 500
-cell_len_y = 500
-num_offset_1 = -5 / cell_len_y
-num_offset_2 = -5 / cell_len_x
-lockBinding("num_cells_1", globalenv())
-lockBinding("num_cells_2", globalenv())
-lockBinding("num_offset_1", globalenv())
-lockBinding("num_offset_2", globalenv())
-lockBinding("cell_len_x", globalenv())
-lockBinding("cell_len_y", globalenv())
+load("prep_RData/grid_mob_one.RData")
+lockBinding("grid", globalenv())
 
 num_nodes = 100L
 num_cells = num_cells_1 * num_cells_2
-num_types = 10L
 lockBinding("num_nodes", globalenv())
 lockBinding("num_cells", globalenv())
-lockBinding("num_types", globalenv())
 
 num_static = 60L
 lockBinding("num_static", globalenv())
 
 gamma_x = 1
 gamma_u = 1
-gamma_y = -1
+gamma_y = -1.5
 
 t_imp_threshold = 1e-4
 
@@ -41,14 +33,8 @@ lockBinding("t_frame", globalenv())
 lockBinding("duration", globalenv())
 
 # mode-specific simulation SETTINGS
-val_k_gas   = 6L
-val_k_audio = 1L
-val_k_photo = 2L
-val_k_wifi  = 1L
-lockBinding("val_k_gas", globalenv())
-lockBinding("val_k_audio", globalenv())
-lockBinding("val_k_photo", globalenv())
-lockBinding("val_k_wifi", globalenv())
+load("prep_RData/impact_multi.RData")
+lockBinding("data_type_spec_df", globalenv())
 
 num_gas_mob = 4L
 lockBinding("num_gas_mob", globalenv())
@@ -60,48 +46,13 @@ lockBinding("p_audio_mob", globalenv())
 lockBinding("p_photo_mob", globalenv())
 lockBinding("p_photo_static", globalenv())
 
-weight_types = c(
-    c(      # gas
-        8,
-        5,
-        5,
-        4,
-        4,
-        3
-    ), c(   # audio
-        5
-    ), c(   # photo
-        7,
-        6
-    ), c(   # wifi
-        3
-    )
-)
-names(weight_types) = paste("d", 1L:num_types, sep = "_")
-lockBinding("weight_types", globalenv())
-
 stopifnot(is.integer(num_cells))
 
 get_t_imp_f = function(t_const) {as.integer(ceiling(t_const * 60 / t_frame))}
 col_type_lt = cumsum(c(1L, val_k_gas, val_k_audio, val_k_photo))
 col_type_rt = cumsum(c(val_k_gas, val_k_audio, val_k_photo, val_k_wifi))
 
-# CONSTRUCT test grid using parameters
-source("lib/square_cell_grid.R")
-
-grid = SquareCellGrid(
-    num_cells_1,
-    num_cells_2,
-    cell_len_x,
-    cell_len_y,
-    num_offset_1,
-    num_offset_2
-)
-
 # CREATE test case elements
-source("lib/element_base.R")
-source("lib/element_multi.R")
-
 capacity_mat = get_capacity_mat_multi(
     val_n           = num_nodes,
     num_static      = num_static,
@@ -114,62 +65,17 @@ capacity_mat = get_capacity_mat_multi(
     p_photo_mob     = p_photo_mob,
     p_photo_static  = p_photo_static
 )
-data_type_spec_df = get_data_type_spec_df_multi(
-    val_k_gas       = val_k_gas,
-    val_k_audio     = val_k_audio,
-    val_k_photo     = val_k_photo,
-    val_k_wifi      = val_k_wifi,
-    weight_original = weight_types
-)
-make_s_impact_f_type_vec(kcl = col_type_lt[1]:col_type_rt[1],
-                         type = "exp", t_const = 550
-)
-make_s_impact_f_type_vec(kcl = col_type_lt[2]:col_type_rt[2],
-                         type = "exp", t_const = 100
-)
-make_s_impact_f_type_vec(kcl = col_type_lt[3],
-                         type = "exp", t_const = 10
-)
-make_s_impact_f_type_vec(kcl = col_type_lt[3] + 1L,
-                         type = "exp", t_const = 750
-)
-make_s_impact_f_type_vec(kcl = col_type_lt[4]:col_type_rt[4],
-                         type = "exp", t_const = 20
-)
-make_t_impact_f_type_vec(kcl = col_type_lt[1]:col_type_rt[1],
-                         type = "step", step = get_t_imp_f(20)
-)
-make_t_impact_f_type_vec(kcl = col_type_lt[2]:col_type_rt[2],
-                         type = "step", step = get_t_imp_f(5)
-)
-make_t_impact_f_type_vec(kcl = col_type_lt[3],
-                         type = "step", step = get_t_imp_f(10)
-)
-make_t_impact_f_type_vec(kcl = col_type_lt[3] + 1L,
-                         type = "step", step = get_t_imp_f(30)
-)
-make_t_impact_f_type_vec(kcl = col_type_lt[4]:col_type_rt[4],
-                         type = "step", step = get_t_imp_f(60)
-)
 local_util_f = get_util_f_type("log_sum")
 
-source("lib/placement_user_locations.R")
-
-create_placement_f = get_create_placement_user_locations_f(
-    data_file = "user_locations_RData/user_locations_yusuf_sample.RData",
-    grid = grid,
-    num_static = num_static
-)
-
+load("prep_RData/mob_300_4_1.RData")
+create_placement_f = get_recreate_placement_user_locations_f(num_static)
 update_placement_f = update_placement_user_locations
 get_placement_f = get_placement_user_locations
 
 source("lib/calc_work_fill_1.R")
-
-calc_work_mat_f = calc_work_mat_fill_1
-
 source("lib/objective_multi.R")
 
+calc_work_mat_f = calc_work_mat_fill_1
 get_objective_f = get_objective_multi_f(
     gamma_x = gamma_x,
     gamma_u = gamma_u,
