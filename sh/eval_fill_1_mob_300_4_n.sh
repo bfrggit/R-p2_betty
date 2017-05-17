a38fe7b7c2204b62b00ae8fea2d27f7a6dd9d531
Qiuxi Zhu <zhuqiuxiuci@gmail.com>
2017-05-17 00:03:10 -0700

    Using one sh script for all

--- sh/eval_fill_1_mob_300_4_n.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-module load R
-cd ~/proj/R-p2/
-Rscript "eval_src/fill_1_mob_300_4_n.R"
--- sh/test_greedy_1_mob_300_4_4.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-module load R
-cd ~/proj/R-p2/
-Rscript "test_src/greedy_1_mob_300_4_4.R"
