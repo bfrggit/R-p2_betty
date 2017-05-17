c47da53f03d97af9f9dfbdf8c2a755ceb0e6ccd6
Qiuxi Zhu <qiuxiz@ics.uci.edu>
2017-05-17 00:10:18 -0700

    Minor fix to p2_job.sh so it runs correctly in other dir

--- p2_job.sh
+++ p2_job.sh
@@ -9,7 +9,7 @@ if [ ! -d $1 ]; then
 	echo >&2 "Error: $1 is not a directory"
 	exit -2
 fi
-
+cd $1
 if [ ! -f $2 ]; then
 	echo >&2 "Error: $2 does not exist"
 	exit -3
@@ -18,5 +18,4 @@ fi
 if type module >/dev/null 2>&1; then
 	module load R
 fi
-cd $1
 Rscript $2
