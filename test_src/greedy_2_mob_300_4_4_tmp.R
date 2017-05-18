b294b0984f49f97e150545b9278860ad1afd6c65
Charles Zhu <qiuxiz@ics.uci.edu>
2017-05-18 13:22:19 -0700

    Added draft of greedy_2 now same with 1

---
+++ test_src/greedy_2_mob_300_4_4_tmp.R
@@ -0,0 +1,22 @@
+cat("Cleaning up the environment...", "\n")
+rm(list = ls())
+cat("\n")
+
+suppressPackageStartupMessages(require(methods))
+
+duration = 180L
+
+source("common/settings.R")
+load("prep_RData/impact_multi_capacity_sm_4.RData")
+load("prep_RData/mob_300_4_4.RData")
+
+source("solution/greedy_2.R")
+
+calc_work_mat_f = get_calc_work_mat_greedy_2_f(
+    gamma_x = gamma_x,
+    gamma_u = gamma_u,
+    gamma_y = gamma_y
+)
+# save_to_file = "tmp/test_greedy_2_mob_300_4_4.RData"
+
+source("common/test_src.R")
