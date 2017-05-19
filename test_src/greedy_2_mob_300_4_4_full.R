adbcfe9043d6b7313eaa4bddb4d3e5acc31bb3dc
Charles Zhu <qiuxiz@ics.uci.edu>
2017-05-18 20:00:10 -0700

    Adding test scripts for greedy_2

--- test_src/greedy_2_mob_300_4_4_full.R
+++ test_src/greedy_2_mob_300_4_4_full.R
@@ -4,19 +4,17 @@ cat("\n")
 
 suppressPackageStartupMessages(require(methods))
 
-data_quota = 3500000L
-
 source("common/settings.R")
 load("prep_RData/impact_multi_capacity_sm_4.RData")
 load("prep_RData/mob_300_4_4.RData")
 
-source("solution/greedy_1.R")
+source("solution/greedy_2.R")
 
-calc_work_mat_f = get_calc_work_mat_greedy_1_f(
+calc_work_mat_f = get_calc_work_mat_greedy_2_f(
     gamma_x = gamma_x,
     gamma_u = gamma_u,
     gamma_y = gamma_y
 )
-save_to_file = "test_data/greedy_1_mob_300_4_4_quota.RData"
+save_to_file = "test_data/greedy_2_mob_300_4_4_full.RData"
 
 source("common/test_src.R")
