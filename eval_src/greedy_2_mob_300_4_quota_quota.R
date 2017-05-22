6fe0b4101bb4df204eb9a3a533333ab63e5140b4
Charles Zhu <qiuxiz@ics.uci.edu>
2017-05-22 14:44:27 -0700

    Plotted quota figs and revised eval_src for quota figs

--- eval_src/greedy_2_mob_300_4_quota_quota.R
+++ eval_src/greedy_2_mob_300_4_quota_quota.R
@@ -4,12 +4,7 @@ cat("\n")
 
 suppressPackageStartupMessages(require(methods))
 
-data_quota = c(
-    seq(      0L,   500000L, by = 100000L),
-    seq( 600000L,  1000000L, by = 200000L),
-    seq(2000000L,  5000000L, by = 500000L),
-    seq(6000000L, 10000000L, by = 1000000L)
-)
+data_quota = as.integer(2 ** seq(16, 23, by = 1 / 3))
 capacity_data_format = "prep_RData/impact_multi_capacity_sm_%d.RData"
 mobility_data_format = "prep_RData/mob_300_4_%d.RData"
 
