29b2cf5fa8a6bec53117a53b173edde43b845858
Charles Zhu <qiuxiz@ics.uci.edu>
2017-05-23 16:05:39 -0700

    Added eval scripts for speed

---
+++ eval_src/greedy_2_mob_300_4_speed_quota.R
@@ -0,0 +1,23 @@
+cat("Cleaning up the environment...", "\n")
+rm(list = ls())
+cat("\n")
+
+suppressPackageStartupMessages(require(methods))
+
+num_nodes = 50L
+gear_rate = seq(0.1, 2, by = 0.1)
+data_quota = 3500000L
+capacity_data_format = "prep_RData/impact_multi_cap_omni_%d.RData"
+mobility_data_format = "prep_RData/mob_300_4_%d.RData"
+
+source("common/settings.R")
+source("solution/greedy_2.R")
+
+calc_work_mat_f = get_calc_work_mat_greedy_2_f(
+    gamma_x = gamma_x,
+    gamma_u = gamma_u,
+    gamma_y = gamma_y
+)
+save_to_file = "eval_data/greedy_2_mob_300_4_speed_quota.RData"
+
+source("common/eval_speed_src.R")
