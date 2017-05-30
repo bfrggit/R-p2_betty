75d14a841fc26190eeb6dbbbdc1a5c9ff266bb0f
Charles Zhu <qiuxiz@ics.uci.edu>
2017-05-30 16:46:34 -0700

    Added scripts to plot eval_mob_x

---
+++ plot_src/plot_all_proc_t_eval_mob_x.R
@@ -0,0 +1,108 @@
+#!/usr/bin/env Rscript
+
+rm(list = ls())
+
+args        = commandArgs(trailingOnly = TRUE)
+args_full   = commandArgs(trailingOnly = FALSE)
+argc        = length(args)
+argc_full   = length(args_full)
+
+if(argc != 1L) {
+    if(argc_full > 0L) {
+        write(
+            paste(c(
+                "Usage:",
+                args_full[1L:(argc_full - argc)],
+                "SUFFIX",
+                "\n"
+            ), collapse = " "),
+            stderr()
+        )
+    }
+    stop("Invalid argument(s)")
+}
+
+mob_suffix = args[1]
+lockBinding("mob_suffix", globalenv())
+
+source("common/plot_mappings.R")
+
+library(ggplot2)
+library(reshape2)
+
+# constants
+exp_set = sprintf("mob_300_4_mob_%s", mob_suffix)
+num_loops = 5L
+frame = 60
+cut_frame = data.frame(x = c(-Inf, +Inf), y = frame)
+
+# load all data files
+data_files = c(
+    "fill_1_%s",
+    "greedy_1_%s_quota",
+    "greedy_2_%s_quota",
+    "greedy_2_%s_full"
+)
+data_f_len = length(data_files)
+data_fr_ls = list()
+
+for(j in 1L:data_f_len){
+    fn = sprintf(data_files[j], exp_set)
+    cat(sprintf("Processing data file eval = \"%s\"", fn), "\n")
+    load(sprintf("eval_data/%s.RData", fn))
+
+    line_cols = c("nodes", "cal")
+    df = data.frame(proc_t_general_avg)
+    df_se = data.frame(proc_t_general_dev) * qnorm(0.975) / sqrt(num_loops)
+    df$nodes = df_se$nodes = as.integer(rownames(proc_t_general_avg))
+    dm = melt(df[, line_cols], id.vars = "nodes", variable.name = "obj")
+    dm_se = melt(df_se[, line_cols], id.vars = "nodes", variable.name = "obj")
+    dm$se = dm_se$value
+    dm$ts = data_files[j]
+    data_fr_ls[[j]] = dm
+}
+dm = do.call("rbind", data_fr_ls)
+
+plot_obj = ggplot(data = dm, aes(x = nodes)) +
+    xlab("Number of mobile nodes") +
+    ylab("Planning time (sec)") +
+    expand_limits(y = 0) +
+    geom_line(
+        aes(x = x, y = y),
+        data = cut_frame, color = "gray40"
+    ) + geom_line(
+        aes(y = value, alpha = ts, color = -value), size = 1
+    ) + geom_errorbar(
+        aes(
+            ymin = value - se,
+            ymax = value + se
+        ), width = (max(df$nodes) - min(df$nodes)) * 0.02,
+        size = 0.5, color = "gray20", alpha = 0.5
+    ) + geom_point(
+        aes(y = value, shape = ts, alpha = ts), size = 2, color = "gray20"
+    ) + scale_y_continuous(
+        limits = c(0, 64)
+    ) + scale_shape_manual(
+        name = pa_sol_name, labels = pa_sol_label, values = pp_sol_shape
+    ) + scale_alpha_manual(
+        name = pa_sol_name, labels = pa_sol_label, values = pa_sol_alpha
+    ) + scale_color_gradientn(
+        colors = rainbow(4, start = 0, end = 0.6), guide = FALSE
+    ) + annotate(
+        "text", label = "Frame", color = "gray40",
+        x = 0,
+        y = frame - 64 / 40
+    )
+cat("Rendering...", "\n")
+
+# plot_obj
+ggsave(
+    filename = sprintf("eval_plot/all_proc_t_mob_%s.pdf", mob_suffix),
+    plot = plot_obj,
+    device = "pdf",
+    width = 8,
+    height = 5,
+    units = "in",
+    dpi = 300
+)
+cat("Done", "\n")
