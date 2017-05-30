260f81e3fa42e19739b317e963024a4a4a42ccbc
Charles Zhu <qiuxiz@ics.uci.edu>
2017-05-30 16:22:36 -0700

    Added scripts for eval mob_c

---
+++ eval_src/fill_1_mob_300_4_mob_c.R
@@ -0,0 +1,17 @@
+cat("Cleaning up the environment...", "\n")
+rm(list = ls())
+cat("\n")
+
+suppressPackageStartupMessages(require(methods))
+
+num_nodes = seq(30L, 200L, by = 10L)
+capacity_data_format = "prep_RData/impact_multi_cap_dist_%d.RData"
+mobility_data_format = "prep_RData/mob_300_4_%d.RData"
+
+source("common/settings.R")
+source("lib/calc_work_fill_1.R")
+
+calc_work_mat_f = calc_work_mat_fill_1
+save_to_file = "eval_data/fill_1_mob_300_4_mob_c.RData"
+
+source("common/eval_mob_c_src.R")
