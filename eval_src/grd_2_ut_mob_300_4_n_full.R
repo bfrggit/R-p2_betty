b2d23fbcc7d652b359f30d92b61b22dd79f6b77f
Charles Zhu <qiuxiz@ics.uci.edu>
2017-06-07 16:54:07 -0700

    Added eval scripts for grd_2_ut, would be slow to run

---
+++ eval_src/grd_2_ut_mob_300_4_n_full.R
@@ -0,0 +1,20 @@
+cat("Cleaning up the environment...", "\n")
+rm(list = ls())
+cat("\n")
+
+suppressPackageStartupMessages(require(methods))
+
+num_nodes = c(seq(5L, 25L, by = 5L), seq(30L, 200L, by = 10L))
+capacity_data_format = "prep_RData/impact_multi_capacity_sm_%d.RData"
+mobility_data_format = "prep_RData/mob_300_4_%d.RData"
+
+source("common/settings.R")
+source("solution/grd_2_ut.R")
+
+calc_work_mat_f = get_calc_work_mat_grd_2_ut_f(
+    gamma_u = gamma_u,
+    gamma_y = gamma_y * 1 * gamma_u / (gamma_x + gamma_u)
+)
+save_to_file = "eval_data/grd_2_ut_mob_300_4_n_full.RData"
+
+source("common/eval_n_src.R")
