4c174eb6f573469c1f0d4502e0bfd97b2be22868
Charles Zhu <qiuxiz@ics.uci.edu>
2017-07-23 13:39:05 -0700

    Added parallel GA test script

---
+++ test_src/ga_1_par_mob_300_4_4_tmp.R
@@ -0,0 +1,24 @@
+cat("Cleaning up the environment...", "\n")
+rm(list = ls())
+cat("\n")
+
+suppressPackageStartupMessages(require(methods))
+
+duration = 180L
+
+source("common/settings.R")
+load("prep_RData/impact_multi_capacity_sm_4.RData")
+load("prep_RData/mob_300_4_4.RData")
+
+source("solution/ga_1.R")
+
+calc_work_mat_f = get_calc_work_mat_ga_1_f(
+    gamma_x = gamma_x,
+    gamma_u = gamma_u,
+    gamma_y = gamma_y,
+    parallel = 6L,
+    seed = 9L
+)
+save_to_file = "tmp/test_ga_1_par_mob_300_4_4.RData"
+
+source("common/test_src.R")
