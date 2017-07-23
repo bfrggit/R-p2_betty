cef5fb816e58f0087724056ca2413880318c308f
Charles Zhu <qiuxiz@ics.uci.edu>
2017-07-23 12:32:51 -0700

    Added even more eval scripts for GA

---
+++ eval_src/ga_1_mob_300_4_mob_b_full.R
@@ -0,0 +1,53 @@
+cat("Cleaning up the environment...", "\n")
+rm(list = ls())
+cat("\n")
+
+args        = commandArgs(trailingOnly = TRUE)
+args_full   = commandArgs(trailingOnly = FALSE)
+argc        = length(args)
+argc_full   = length(args_full)
+
+if(argc != 1L) {
+    if(argc_full > 0L) {
+        write(
+            paste(c(
+                "Usage:",
+                args_full[1L:(argc_full - argc)],
+                "X_CASE",
+                "\n"
+            ), collapse = " "),
+            stderr()
+        )
+    }
+    stop("Invalid argument(s)")
+}
+
+x_case = as.integer(args[1])
+stopifnot(x_case == as.numeric(args[1]))
+
+suppressPackageStartupMessages(require(methods))
+
+num_mob_all = seq(0L, 50L, by = 2L) # length = 26
+stopifnot(length(num_mob_all) >= x_case)
+
+num_nodes = 50L
+num_mob = num_mob_all[x_case]
+capacity_data_format = "prep_RData/impact_multi_cap_omni_%d.RData"
+mobility_data_format = "prep_RData/mob_300_4_%d.RData"
+
+source("common/settings.R")
+source("solution/ga_1.R")
+
+out_path = "eval_data/ga_1_mob_300_4_mob_b_full"
+if(!dir.exists(out_path))
+    dir.create(out_path, recursive = TRUE, mode = "0775")
+
+calc_work_mat_f = get_calc_work_mat_ga_1_f(
+    gamma_x = gamma_x,
+    gamma_u = gamma_u,
+    gamma_y = gamma_y,
+    seed = 9L
+)
+save_to_file = sprintf("%s/case_%d.RData", out_path, x_case)
+
+source("common/eval_mob_b_src.R")
