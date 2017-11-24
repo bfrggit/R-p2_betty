ff57a4a816c753a60a2e4912b9014e02e6a00d84
Qiuxi Zhu <qiuxiz@ics.uci.edu>
2017-11-24 00:17:08 -0800

    Reran simulations for lyap_grd using 1e-7 as gamma_l and corrected func

--- test_src/lyap_grd_mob_300_4_4_full.R
+++ test_src/lyap_grd_mob_300_4_4_full.R
@@ -14,7 +14,7 @@ calc_work_mat_f = get_calc_work_mat_lyap_grd_f(
     gamma_x = gamma_x,
     gamma_u = gamma_u,
     gamma_y = gamma_y,
-    gamma_l = 5e-9
+    gamma_l = 1e-7
 )
 save_to_file = "test_data/lyap_grd_mob_300_4_4_full.RData"
 
